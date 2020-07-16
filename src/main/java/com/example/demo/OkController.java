1325e95d80340ff7f59db444e1453d1b80cf81c1
vbalan <vbalan@patriot.ambianta.local>
2020-07-16 09:27:12 +0300

    with omega page

--- src/main/java/com/example/demo/OkController.java
+++ src/main/java/com/example/demo/OkController.java
@@ -19,4 +19,24 @@ public class OkController {
   public String badPage() {
   	return "Oooops!!!";
   }
+
+@RequestMapping("/omega")
+@ResponseBody
+  public String omegaPage() {
+  	return "<title>Opel Omega</title>"
+  		 + "<h1>MY OPEL OMEGA B</h1>"
+  		 + "<p>1994 - 2003</p>"
+  		 + "<p>X20SE - 116 ps </p>"
+  		 + "<p>X20XEV - 136 ps </p>"
+  		 + "<p>X2.2XEV - 144 ps </p>"
+ 		 + "<p>X2.5XEV - 170 ps </p>"
+ 		 + "<p>Y2.6XEV - 177 ps</p>"
+ 		 + "<p>U3.0XEV - 211 ps</p>"
+ 		 + "<p>Y3.2XEV - 218 ps </p>"
+ 		 + "<h3>Very good automobile</h3>"
+  		 + "<p><img src=\"https://i.piccy.info/i9/08bd9ce9799b4764822f97bca94eb358/1423302543/78872/714646/_21ByL_295ugCWk_7E_28KGrHqJ_21iYEw5C17YTWBMQumsnfqw_7E_7E_3.jpg\" alt=\"Вид заголовка\" width=\"600\" height=\"450\"></p>"
+  		 + "<p><a href=\"https://www.google.com/search?q=opel+omega+image&safe=active&client=firefox-b-d&sxsrf=ALeKk00lrF89Jk1GIxQ-wcylsXIAf2Ad9A:1594880069065&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjz_eafj9HqAhVys4sKHaN0AJEQ_AUoAXoECDQQAw&biw=1920&bih=966\">Image Omega</a></p>"
+  		 + "<p><a href=\"https://ru.wikipedia.org/wiki/Opel_Omega\">About Omega</a></p>"
+  		 + "<p><a href=\"http://localhost:8080/ok\">ok</a></p>";
+  }
 }
