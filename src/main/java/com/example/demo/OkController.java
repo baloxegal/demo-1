0c740f7f5fc05586213bafd21e750e4a2fa6f9e8
Your Name <you@example.com>
2020-07-16 21:39:06 +0300

    with css fail

--- src/main/java/com/example/demo/OkController.java
+++ src/main/java/com/example/demo/OkController.java
@@ -1,13 +1,9 @@
 package com.example.demo;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Properties;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
